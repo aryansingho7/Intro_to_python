4b4b06039407e252c35a79802e05af8bd387e718
Aryan <aryanprime5411@gmail.com>
2021-04-17 22:18:40 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,25 @@
+#example of bitwise operators
+
+a = 10
+b = 4
+
+#AND
+print('a & b= ', a & b)
+
+#OR
+print('a|b=',a|b)
+
+#NOT
+print('-a', -a)
+
+#XOR
+print('a^b=', a^b)
+print('b^a=', b^a)
+
+#Right Shift
+print('a >> 1', a>>1)
+print('b>>1', b>>1)
+
+#left shift
+print('a <<1', a<<1)
+print('b<<1', b<<1)
