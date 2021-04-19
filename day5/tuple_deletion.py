ae0b5551f4b7aede4c6993f346d8e61182e111c7
Aryan <aryanprime5411@gmail.com>
2021-04-19 20:37:25 +0530

    day5

---
+++ day5/tuple_deletion.py
@@ -0,0 +1,4 @@
+#deleting a tuple
+tuple1=(0,1,2,3,4)
+del tuple1
+print(tuple1)
---
+++ day5/tuples_slicing.py
@@ -0,0 +1,15 @@
+#slicing of tuples
+#with numbers
+tuple1= tuple('geeksforgeek')
+
+#removing first element 
+print('removing the first element:')
+print(tuple1[1:])
+
+#reversing the tuple
+print('reverse tuple:')
+print(tuple1[::-1])
+
+#printing element of a range
+print('/n Element sof ranc ebetween 4 to 5')
+print(tuple1[4:9])
