1501c789384e066d21ffa31362c028e1d84e6bd4
Aryan <aryanprime5411@gmail.com>
2021-07-14 23:37:26 +0530

    arrays

--- Problems/Arrays/two sums.py
+++ Problems/Arrays/two sums.py
@@ -1,20 +1,16 @@
-arr1= [1,3,5,7]
-target1= 8
-
-arr2=[1,8,9,3]
-target2= 5
-
-def two_sum(arr, target):
-    values= dict()
-    for i, elem in enumerate(arr):
-        comp=target-elem
-        if comp in values:
-            return [values[comp],i]
-        values[elem]=i
-    return[]
-
-list1= two_sum(arr1,target1)
-print(list1)
-
-list2= two_sum(arr2, target2)
-print(list2)
+class Solution(object):
+   def twoSum(self, nums, target):
+      """
+      :type nums: List[int]
+      :type target: int
+      :rtype: List[int]
+      """
+      required = {}
+      for i in range(len(nums)):
+         if target - nums[i] in required:
+            return [required[target - nums[i]],i]
+         else:
+            required[nums[i]]=i
+input_list = [2,8,12,15]
+ob1 = Solution()
+print(ob1.twoSum(input_list, 20))
