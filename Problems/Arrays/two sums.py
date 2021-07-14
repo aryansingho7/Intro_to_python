c954a4b406b4c65202f5675d9e51ab3055e1e0b9
Aryan <aryanprime5411@gmail.com>
2021-07-14 22:55:13 +0530

    arrays

--- Problems/Arrays/two sums.py
+++ Problems/Arrays/two sums.py
@@ -1,13 +1,20 @@
-def twoSum(nums, target):
-    """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-    h = {}
-    for i, num in enumerate(nums):
-        n = target - num
-        if n not in h:
-            h[num] = i
-        else:
-            return [h[n], i]
+arr1= [1,3,5,7]
+target1= 8
+
+arr2=[1,8,9,3]
+target2= 5
+
+def two_sum(arr, target):
+    values= dict()
+    for i, elem in enumerate(arr):
+        comp=target-elem
+        if comp in values:
+            return [values[comp],i]
+        values[elem]=i
+    return[]
+
+list1= two_sum(arr1,target1)
+print(list1)
+
+list2= two_sum(arr2, target2)
+print(list2)
