da4ebad9c8474b19fde4b97db61d5a60c75b6b41
Aryan <aryanprime5411@gmail.com>
2021-07-15 23:32:15 +0530

    arrays

--- Problems/Arrays/two sums.py
+++ Problems/Arrays/two sums.py
@@ -1,16 +1,8 @@
 class Solution(object):
    def twoSum(self, nums, target):
-      """
-      :type nums: List[int]
-      :type target: int
-      :rtype: List[int]
-      """
-      required = {}
-      for i in range(len(nums)):
-         if target - nums[i] in required:
-            return [required[target - nums[i]],i]
-         else:
-            required[nums[i]]=i
-input_list = [2,8,12,15]
-ob1 = Solution()
-print(ob1.twoSum(input_list, 20))
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return [i,j]
+    
