bf0aa9897c479149f44f8f7871ed83b811077399
Aryan <aryanprime5411@gmail.com>
2021-08-31 17:04:35 +0530

    gfg_quest

---
+++ Problems/gfg/max_of_two_num.py
@@ -0,0 +1,29 @@
+#python program to find the max of two numbers
+#METHORD 1 DEFINING FUCTION 
+"""def man(a,b):
+    if a>=b:
+        return a
+    else:
+        return b
+
+#driver code 
+a= 2
+b=6
+print(man(a,b))"""
+
+#METHORD TWO USING THE MAX FUCTION 
+"""a=2
+b=3
+
+maximum=max(a,b)
+print(maximum)"""
+
+
+
+#METHORD USING TERNARY OPERATOR
+
+#driver code
+a=2
+b=4
+
+print(a if a>=b else b)
