c975c576c0991d8f62a4ea4a9d087c24eecfa6e1
Aryan <aryanprime5411@gmail.com>
2022-02-28 21:21:59 +0530

    problem 3

--- 450/array/3_FindKMaxandMin.py
+++ 450/array/3_FindKMaxandMin.py
@@ -4,5 +4,27 @@ def kthsmallest(arr,k):
     return arr[k-1]
 arr= list(map(int,input().split(' ')))
 k= int(input())
-kthsmallest((arr,k))
+kthsmallest(arr,k)
 
+def kthlargest(arr,k):
+    k=len(arr)-k
+    def quickselect(l,r):
+        pivot, p= arr[r], l
+        for i in range(l,r):
+            if arr[i] <= pivot:
+                arr[p], arr[i]= arr[i], arr[p]
+                p += 1
+        arr[p], arr[r]= arr[r], arr[p]
+
+        if p>k:
+            return quickselect(l,p-1)
+        elif p <k:
+            return quickselect(p+1,r)
+        else:
+            return arr[p]
+
+    return quickselect(0, len(arr)-1)
+
+arr =list(map(int,input().split(' ')))
+k=int(input())
+kthlargest(arr,k)
